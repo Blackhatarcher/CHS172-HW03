970e73f672bf3d518f6d2a413f482d2c9564161a
Blackhat-PC\Blackhat <christianwilliams20@my.whitworth.edu>
2016-09-30 23:57:48 -0700

    and done ish

---
+++ 10.10/10.10/10.10.cpp
@@ -0,0 +1,73 @@
+// 10.10.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class MyInteger
+{
+private:
+
+	int value;
+
+public:
+	int getValue() const{
+		return value;
+	}
+
+	MyInteger(int value){
+		this->value = value;
+		//using pointer 'this' to retrieve int value//
+	}
+
+	bool isPrime() const{
+		return isPrime(value);
+	}
+	static bool isPrime(const MyInteger& o){
+		return isPrime(o.getValue());
+	}
+	//following code will be used to determine whether number is prime or not//
+	static bool isPrime(int num)
+	{
+		if ((num == 1) || (num == 2))
+		{
+			return true;
+		}
+		for (int i = 2; i <= num / 2; i++)
+		{
+			if (num % i == 0)
+				return false;
+		}
+		return true;
+	}
+	bool isEven() const
+	{
+		return isEven(value);
+	}
+	static bool isEven(int n)
+	{
+		return n % 2 == 0;
+	}
+	static bool isEven(const MyInteger& n)
+	{
+		return isEven(n.getValue());
+	}
+	bool equals(int anotherNum) const{
+		return value == anotherNum;
+	}
+	bool equals(const MyInteger& o) const{
+		return value == o.getValue();
+	}
+};
+int main()
+{
+	MyInteger n1(5);
+	cout << "Is the number even? " << n1.isEven() << endl;
+	cout << "Is the number prime? " << n1.isPrime() << endl;
+	cout << "Is 5 prime? " << MyInteger::isPrime(5) << endl;
+
+	return 0;
+}
+
---
+++ 10.4/10.4/10.4.cpp
@@ -0,0 +1,46 @@
+// 10.4.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string sort(string& s)
+{  //following lines of code will rearrange letters input by user//
+	for (int i = s.length() - 1; i >= 1; i--)
+	char currentMax = s[0];
+	int currentMaxIndex = 0;
+	for (int j = 1; j <= i; j++)
+	{
+		if (currentMax < s[j])
+		{
+			currentMax = s[j];
+			currentMaxIndex = j;
+		}
+	}
+	// following line of code switches 'i' with 'currentMaxIndex'//
+	if (currentMaxIndex != i)
+	{
+		s[currentMaxIndex] = s[i];
+		s[i] = currentMax;
+	}
+}
+
+return s;
+
+}
+
+int main()
+{
+	//prompts user to input string//
+	cout << "Enter a string: ";
+	string s;
+	getline(cin, s);
+
+	//returns the sorted string// 
+	cout << "The sorted string is: " << sort(s) << endl;
+
+	return 0;
+}
